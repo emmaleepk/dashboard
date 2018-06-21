87fb77cc167ae0240c3db2b7a57971ee007380a8
emmaleepk <emmaleepk@gmail.com>
2018-06-20 21:16:46 -0500

    New beginnings

---
+++ Server/index.js
@@ -0,0 +1,99 @@
+const { GraphQLServer } = require('graphql-yoga');
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://emmaleepk:EPK123@ds237770.mlab.com:37770/gymdb');
+var db = mongoose.connection;
+
+// User Model
+const User = mongoose.model('User', {
+  firstName: {
+    type: String,
+    default: ''
+  },
+  lastName: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: '',
+    lowercase: true
+  },
+  password: {
+    type: String,
+    default:''
+  },
+  isDeleted: {
+    type: Boolean,
+  },
+  classTaken: {
+    type: Object,
+    properties:{
+      classType: {
+        type: Array,
+        items:{
+          type: String,
+          default: ''
+        }
+      },
+      instructor:{
+        type: Array,
+        items:{
+          type: String,
+          default: ''
+        }
+      },
+      date:{
+        type: Array,
+        items:{
+          type: Date,
+          default: Date.now
+        }
+      }
+    }
+  }
+});
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    isDeleted: Boolean!,
+    classTaken: {[ClassType], [instructor], [date]}
+  }
+  type ClassType {
+
+  }
+  type Mutation {
+    createUser(firstName: String!,
+               lastName: String!,
+               email: String!,
+               password: String!): User
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+  Mutation: {
+    createUser: async(_, {firstName, lastName, email, password}) => {
+      const user = new User({ firstName, lastName, email, password, isDeleted: false });
+      await user.save();
+      return user;
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  server.start(() => console.log('Server is running on localhost:4000'));
+});
